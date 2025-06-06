a59eb479ce88c63ec293f2cf6040796914742c5d
gilberto-domingos <domingoshot@hotmail.com>
2025-06-06 16:00:47 -0300

    chore: praticing fake data Bogus and config

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace TipsConsole {
-    internal class Program {
-        static void Main(string[] args) {
-            var examples = new Examples();
+﻿using DataLib;
 
-            examples.RunExample();
+namespace TipsConsole;
 
-        }
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var examples = new Examples();
+        examples.RunExample();
+
+        var repo = new DataRepository();
+
+        Console.WriteLine("=== CUSTOMERS ===");
+        foreach (var customer in repo.Customers)
+            Console.WriteLine(
+                $"{customer.FirstName} {customer.LastName} - {customer.EmailAddress} - {customer.Company} - Desde {customer.CustomerSince}");
+
+        Console.WriteLine("\n=== SONGS ===");
+        foreach (var song in repo.Songs)
+            Console.WriteLine($"{song.SongTitle} - {song.Genre} - {song.Artist} - {song.Label}");
+
+        Console.WriteLine("\n=== PRODUCTS ===");
+        foreach (var product in repo.Products)
+            Console.WriteLine($"{product.Name} ({product.Color}) - {product.Description} - {product.Department}");
     }
 }
