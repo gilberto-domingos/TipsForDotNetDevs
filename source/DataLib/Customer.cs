22870e007c7f9db83ce9331022ac2cc20e975ca8
gilberto-domingos <domingoshot@hotmail.com>
2025-06-06 15:50:07 -0300

    Refactor: praticing fake data Bogus library

--- source/DataLib/Customer.cs
+++ source/DataLib/Customer.cs
@@ -1,6 +1,7 @@
 ﻿namespace DataLib {
   public class Customer {
-    public string FullName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string EmailAddress { get; set; }
     public string Company { get; set; }
     public DateOnly CustomerSince { get; set; }
--- source/DataLib/DataRepository.cs
+++ source/DataLib/DataRepository.cs
@@ -10,46 +10,102 @@ namespace DataLib {
     public DataRepository() {
      
     }
-    public List<Customer> GetCustomerData() {
-      _customerList = new List<Customer>();
-      _customerList.Add(new Customer { FullName = "Walt Ritscher", EmailAddress = "walt@somewhere.com", Company = "LinkedIn", CustomerSince = DateOnly.FromDateTime(new DateTime(year: 2015, month: 11, day: 30)) });
-      _customerList.Add(new Customer { FullName = "Richard Wallace", EmailAddress = "richard@somewhere.com", Company = "Big Star Collectibles", CustomerSince = DateOnly.FromDateTime(new DateTime(year: 2021, month: 4, day: 12)) });
-      return _customerList;
-    }
+    public List<Customer> GetCustomerData()
+{
+    _customerList = new List<Customer>();
+
+    _customerList.Add(new Customer 
+    { 
+        FirstName = "Walt", 
+        LastName = "Ritscher",
+        EmailAddress = "walt@somewhere.com", 
+        Company = "LinkedIn", 
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2015, 11, 30)) 
+    });
+    _customerList.Add(new Customer 
+    { 
+        FirstName = "Richard", 
+        LastName = "Wallace", 
+        EmailAddress = "richard@somewhere.com", 
+        Company = "Big Star Collectibles", 
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2021, 4, 12)) 
+    });
+
+    _customerList.Add(new Customer
+    {
+        FirstName = "Alice",
+        LastName = "Johnson",
+        EmailAddress = "alice.johnson@example.com",
+        Company = "Tech Solutions",
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2019, 6, 5))
+    });
+    _customerList.Add(new Customer
+    {
+        FirstName = "Bob",
+        LastName = "Smith",
+        EmailAddress = "bob.smith@example.com",
+        Company = "Creative Co.",
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2018, 9, 20))
+    });
+    _customerList.Add(new Customer
+    {
+        FirstName = "Carol",
+        LastName = "Miller",
+        EmailAddress = "carol.miller@example.com",
+        Company = "Innovatech",
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2022, 1, 15))
+    });
+    _customerList.Add(new Customer
+    {
+        FirstName = "David",
+        LastName = "Brown",
+        EmailAddress = "david.brown@example.com",
+        Company = "Business Corp",
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2020, 7, 23))
+    });
+    _customerList.Add(new Customer
+    {
+        FirstName = "Eva",
+        LastName = "Davis",
+        EmailAddress = "eva.davis@example.com",
+        Company = "Enterprise Inc.",
+        CustomerSince = DateOnly.FromDateTime(new DateTime(2017, 3, 8))
+    });
+
+    return _customerList;
+}
+
+    // public List<Customer> Customers
+    // {
+    //   get
+    //   {
+    //     // code to access data in database
+    //     return GetCustomerData();
+    //
+    //
+    //   }
+    // }
+
     public List<Customer> Customers
     {
-      get
+      get   
       {
-        // code to access data in database
-        return GetCustomerData();
+        var fakerList = new List<Customer>();
+        var customerFaker = new Bogus.Faker<DataLib.Customer>();
+        customerFaker.RuleFor(x => x.FirstName, x => x.Person.FullName);
+        customerFaker.RuleFor(x => x.LastName, x => x.Person.LastName);
+        customerFaker.RuleFor(x => x.EmailAddress, x => x.Person.Email);
+        customerFaker.RuleFor(x => x.Company, x => x.Person.Company.Name);
+        customerFaker.RuleFor(x => x.CustomerSince, x => DateOnly.FromDateTime(x.Date.Past(5, DateTime.Now)));
+        customerFaker.RuleFor(x => x.Review, x => x.Rant.Review());
 
-
+        for (int counter = 0; counter < 30; counter++)
+        {
+          fakerList.Add(customerFaker.Generate());
+        }
+          return fakerList;
       }
     }
-
-    //public List<Customer> Customers
-    //{
-    //  get
-    //  {
-
-
-    //    var fakerList = new List<Customer>();
-    //    var customerFaker = new Bogus.Faker<DataLib.Customer>();
-    //    customerFaker.RuleFor(x => x.FullName, x => x.Person.FullName);
-    //    customerFaker.RuleFor(x => x.EmailAddress, x => x.Person.Email);
-    //    customerFaker.RuleFor(x => x.Company, x => x.Person.Company.Name);
-    //    customerFaker.RuleFor(x => x.CustomerSince, x => DateOnly.FromDateTime(x.Date.Past(5, DateTime.Now)));
-    //    customerFaker.RuleFor(x => x.Review, x => x.Rant.Review());
-
-    //    for (int counter = 0; counter < 30; counter++)
-    //    {
-    //      fakerList.Add(customerFaker.Generate());
-    //    }
-
-    //    return fakerList;
-
-    //  }
-    //}
 
     public List<Song> Songs
     {
@@ -97,5 +153,5 @@ namespace DataLib {
         return fakerList;
       }
     }
-  }
+  } 
 }
